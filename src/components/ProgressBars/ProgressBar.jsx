e976982f1a67e27134aa9956a7cfafbf02ec2df4
Mohit <mohit2412gupta@gmail.com>
2023-11-10 15:51:36 +0530

    changes

--- src/components/ProgressBars/ProgressBar.jsx
+++ src/components/ProgressBars/ProgressBar.jsx
@@ -7,17 +7,6 @@ const ProgressBar = ({ questionSolved, value, height, width }) => {
     height: `${height}px`,
   });
 
-  // const newBarStyle = ;
-  // setBarStyle(newBarStyle);
-  // setTimeout(() => {
-  //   const newStyle = {
-  //     opacity: 1,
-  //     width: `${value}%`,
-  //   };
-
-  //   setStyle(newStyle);
-  // }, 1000);
-
   return (
     <div className="progress" style={barStyle}>
       <div
@@ -25,12 +14,7 @@ const ProgressBar = ({ questionSolved, value, height, width }) => {
         style={{ width: `${value}%`, "--progress-width": `${value}%` }}
       ></div>
     </div>
-    // <div className=" bg-[#d8d8d8] rounded-lg w-3/5 h-6 " style={barStyle}>
-    //   <div
-    //     className=" rounded-lg bg-gradient-to-r from-[#74D680] to-[#378B29]   text-white flex items-center justify-center h-full opacity-0 transition-opacity-1000-ease-300"
-    //     // style={style}
-    //   ></div>
-    // </div>
+    
   );
 };
 
