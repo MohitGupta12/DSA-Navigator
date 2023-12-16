e221e5715a4b9c18cc5d5afb0fbfeb6c1aa7cc65
Mohit <mohit2412gupta@gmail.com>
2023-12-17 00:07:08 +0530

    Some Changes

--- src/Animations/ScrollAnimation.jsx
+++ src/Animations/ScrollAnimation.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { motion } from "framer-motion";
 
 const ScrollAnimation = () => {
   return (
     <div className="absolute flex items-center justify-center w-full xs:bottom-10 bottom-16">
-      <div className=" w-8 xl:w-9  h-14 xl:h-16 rounded-3xl border-4 border-[#F0FFFF] flex justify-center item-start p-2">
+      <div className=" w-8 xl:w-9  h-14 xl:h-16 rounded-3xl border-4 border-scrollAnimation flex justify-center item-start p-2">
         <motion.div
           animate={{ y: [0, 24, 0] }}
           transition={{
@@ -12,7 +11,7 @@ const ScrollAnimation = () => {
             repeat: Infinity,
             repeatType: "loop",
           }}
-          className="w-2 h-2 xl:h-3 xl:w-2 mb-1 bg-[#F0FFFF] rounded-full"
+          className="w-2 h-2 xl:h-3 xl:w-2 mb-1 bg-scrollAnimation rounded-full"
         />
       </div>
     </div>
