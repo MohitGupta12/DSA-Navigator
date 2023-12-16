140029cef3a7fa987066ac1c82226ab155b399bc
Mohit <mohit2412gupta@gmail.com>
2023-12-17 00:32:39 +0530

    New Routes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,19 +1,36 @@
 import BottomNavBar from "../BottomNavBar/BottomNavBar";
-import { Link, Outlet } from "react-router-dom";
+import { Link, useLocation, Route, Routes } from "react-router-dom";
 import { SharedDataProvider } from "../../Hooks/Context/useSharedData";
-import { Toaster} from 'sonner'
-
+import { Toaster } from "sonner";
+import { Tooltip, Zoom } from "@mui/material";
+import { AnimatePresence } from "framer-motion";
+import HomePage from "../../Pages/HomePage";
+import QuestionPage from "../../Pages/QuestionPage.jsx";
+import AboutPage from "../../Pages/AboutPage.jsx";
 
 function Layout() {
-  return (
-    <>      
-    <Toaster richColors  position="top-right" />
+  const location = useLocation();
 
-      <SharedDataProvider >
-        <h1 className="mt-6 mb-2 text-4xl font-bold text-center md:text-5xl  " style ={{"textShadow": "0px 0px 16px rgba(255,255,255,0.71) "}}>
-          <Link to={"/"}>DSA Tracker</Link>
+  return (
+    <>
+      <Toaster richColors position="top-right" />
+
+      <SharedDataProvider>
+        <h1
+          className="mt-6 mb-2 text-4xl font-bold text-center md:text-5xl  "
+          style={{ textShadow: "0px 0px 6px rgba(255,255,255,0.71) " }}
+        >
+          <Tooltip TransitionComponent={Zoom} title="Home" arrow placement="right">
+            <Link to={"/"}>DSA Tracker</Link>
+          </Tooltip>
         </h1>
-        <Outlet />
+        <AnimatePresence mode="wait">
+          <Routes key={location.pathname} location={location} >
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="questions/:id" element={<QuestionPage />} />
+            <Route path="about" element={<AboutPage />} />
+          </Routes>
+        </AnimatePresence>
         <BottomNavBar />
       </SharedDataProvider>
     </>
